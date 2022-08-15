80a7561d6cc23ab8e5bc0915d46ef8360b75f332
thevaliantjosh <joshuacalebsprouse@gmail.com>
2022-08-14 20:29:00 -0500

    Data Types, Conditionals, JavaScript and HTML

--- js/javascript_with_html.js
+++ js/javascript_with_html.js
@@ -1,16 +1,50 @@
+"use strict";
+
+////alerts will show a dialog box with the included message
 //alert("Hello, World!");
+//
+///*"confirm" lets you perform a certain action based on if the user
+//chooses to Ok === true  or Cancel === false*/
 //let userChoice = confirm("Are you sure you don't want to buy stuff?");
-
-
+//
 //if (userChoice === true) {
 //    alert("Yay!");
 //    } else {
 //        alert("Booo :(");
 //     }
+//
+//
+////prompt
+//
+//let userName = prompt("What's your Name?");
+//alert("Welcome " + userName + "!" )
+//
+//console.log("The users name is: " + userName)
+
+//Exercise #4: use the following code to follow the instructions below:
+
+//var username = 'codeup';
+//var password = 'notastrongpassword'
+
+//Create a variable that holds a boolean value for each of the following conditions:
+
+//the password must be at least 5 characters
+//the password must not include the username
+//the username must be no more than 20 characters
+//neither the username or password can start or end with whitespace
 
 
-//prompt
 
-prompt(message:"What's your Name?");
+//    let enterUserName = prompt("Please enter your username: ");
+//    function userNameLength() {
+//        if (enterUserName.length > 20) {
+//            alert("Please enter a username less than 20 characters!")
+//            }  else if (enterUserName.length > 20) {
+//                enterUserName();
+//            }
+//            else (console.log("user name entered: " + enterUserName));
+//
+//    }
+//
+// userNameLength();
 
-//console.log(userChoice);
