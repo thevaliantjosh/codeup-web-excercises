838dc5e0b552b98fe10b3056ac4b097444152dc6
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-08-30 07:36:48 -0500

    Completed Introduction to CSS Exercise

--- js/warmups.js
+++ js/warmups.js
@@ -112,12 +112,23 @@ console.log(calculateTotalStudents(classes));
 
 //convertToObject("Intro to Programming", 20) returns {class: "Intro to Programming", students: 20}
 
-function convertToObject(string, number){
-    return {class: string, number: number};
+// function convertToObject(nameOfClass, numberOfStudents){
+//     return {
+//         class: nameOfClass,
+//         numberOfStudents: numberOfStudents
+//     };
+// }
+
+const convertToObject = (nameOfClass, numberOfStudents) => {
+    return {
+        class: nameOfClass,
+        numberOfStudents: numberOfStudents
+    }
 }
 
+console.log(convertToObject("Intro to Programming", 20));
 
-var neighborhood1 ={
+const neighborhood1 ={
     neighborhood: "Lovely Estates",
     medianHomePrice: 280000,
     pool: true,
@@ -130,7 +141,7 @@ var neighborhood1 ={
     ]
 }
 
-var neighborhood2 ={
+const neighborhood2 ={
     neighborhood: "Luminous Estates",
     medianHomePrice: 270000,
     pool: true,
@@ -143,7 +154,7 @@ var neighborhood2 ={
     ]
 }
 
-var neighborhood3 ={
+const neighborhood3 ={
     neighborhood: "Ginormous Ego Estates",
     medianHomePrice: 350000,
     pool: true,
@@ -158,13 +169,24 @@ var neighborhood3 ={
 
 // Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
 
-function desirableNeighborhood(neighborhoodObject){
+function desirableNeighborhood(neighborhoodObject) {
     let totalRating = 0;
-    for (let i = 0; i < neighborhoodObject.schools.length; i++){
+    for (let i = 0; i < neighborhoodObject.schools.length; i++) {
         totalRating += neighborhoodObject.schools[i].rating;
     }
     return neighborhoodObject.medianHomePrice < 300000 && neighborhoodObject.crimeRate === "low" && totalRating >= 24;
+
 }
+
+
+
+// function desirableNeighborhood(neighborhoodObject){
+//     let totalRating = 0;
+//     for (let i = 0; i < neighborhoodObject.schools.length; i++){
+//         totalRating += neighborhoodObject.schools[i].rating;
+//     }
+//     return neighborhoodObject.medianHomePrice < 300000 && neighborhoodObject.crimeRate === "low" && totalRating >= 24;
+// }
 
 //Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
 //
@@ -180,3 +202,109 @@ function desirableNeighborhood(neighborhoodObject){
 //
 // beeramid(1500, 2); // should === 12
 // beeramid(5000, 3); // should === 16
+
+/*Whats the cost of each level level*level*priceOfCan
+* each level costs the amount of cans * the price of the can
+* The amount of cans in each level will be the square of the number of the level
+* (you could have a recursive solution here)
+* calculate each cost of level and refer to the bonus
+* continue;
+* calculate cost of next level then add to the previous
+* Need a totalCost accumulator variable and some kind of level variable that keeps track of what level I'm on and I need to keep looping unitl totalCost > referral bonus.
+*
+*
+* */
+
+
+function beeramid(bonus, price) {
+    let totalCost = 0;
+    let levels = 0;
+    while(totalCost < bonus) {
+        totalCost += levels * levels * price;
+        if(totalCost + ((levels + 1) **2 * price) >bonus){
+            break;
+        }
+        levels++;
+
+    }
+    return levels;
+}
+
+// function beeramid (bonus,price){
+//     let total = (bonus - (bonus % price))/price;
+//     let x = 0;
+//     for (let i = 0; total >= ((1 / 3)*(i**3))+((1 / 2)*(i**2))+((1 / 6)*(i)); i++){
+//         x = i;
+//     }
+//     return x;
+// }
+//
+// function beeramid (bonus,price){
+//     let total = (bonus - (bonus % price))/price; //this line tells you how many cans of beer you can buy
+//     let x = 0; //accumulator variable
+//     for (let i = 0; total >= ((1 / 3)*(i**3))+((1 / 2)*(i**2))+((1 / 6)*(i)); i++){ //solved for a cubic function
+//         x = i; //match the acumulator to the number of levels
+//     }
+//     return x;
+// }
+
+// Write a function, getSimpleUser, that takes in a user object and returns a simplified user object.
+//
+//     Assume the following shape of the user object for the function input:
+//
+//     ```
+// {
+//   firstName: '...',
+//   lastName: '...',
+//   email: '...',
+//   username: '...',
+//   password: '...'
+// }
+// ```
+//
+// The shape of the output should be the following:
+//
+//     ```
+// {
+//   firstName: '...',
+//   lastName: '...',
+// }
+//
+// Regardless of the case of the first name and last name input, the output should be in all lower case.
+
+var fred = {
+    firstName: 'Fred',
+    lastName: 'Smith',
+    email: 'fred@email.com',
+    username: 'fred123',
+    password: 'pass123'
+}
+getSimpleUser(fred) // {firstName: 'fred', lastName: 'smith'}
+
+// Create a function, createNames, that takes in an array of first names and an array of last names. The function should return an array of name objects with firstName and lastName properties with the input array values. Assume both input arrays are not empty, the same length and only contain string elements.
+//
+//     var firstNames = ['CJ', 'Max', 'Claude', 'Meowmeow'];
+// var lastNames = ['Cat', 'Feline', 'Kitten', 'Calico'];
+//
+// createNames(firstNames, lastNames) // returns...
+//
+//     [
+//     {
+//         firstName: 'CJ',
+//         lastName: 'Cat'
+//     },
+//         {
+//             firstName: 'Max',
+//             lastName: 'Feline'
+//         },
+//         {
+//             firstName: 'Claude',
+//             lastName: 'Kitten'
+//         },
+//         {
+//             firstName: 'Meowmeow',
+//             lastName: 'Calico'
+//         }
+//     ]
+//
+// HINT: a solution may involve using a for loop to iterate over the length of one array to push each name object onto an array of name objects that will be returned from the function.
