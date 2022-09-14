785393bf7ba4038796618ea7076b2b43c9c6e61a
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-09-14 15:02:51 -0500

    defuse the bom

--- js/browser-object-model-lecture.js
+++ js/browser-object-model-lecture.js
@@ -8,9 +8,46 @@ let sayHey = function() {
         if (count < 10) {
             console.log("hey" + count);
             count++
+        } else {
+            clearInterval(theInterval);
         }
     }
-
-    setInterval(hey, 1000)
+    let theInterval= setInterval(hey, 1000)
 }
 
+// let count = 0;
+// let thisTimeItWillStop;
+// function hey() {
+//     if (count < 10) {
+//         console.log("hey " + count);
+//         count++
+//     } else {
+//         clearInterval(thisTimeItWillStop);
+//     }
+// }
+// thisTimeItWillStop = setInterval(hey, 1000);
+
+function tenHeys() {
+    let count = 0;
+    let thisWillStop = setInterval(function(){
+        if (count < 10) {
+            console.log("hey " + count);
+            count++
+        } else {
+            clearInterval(thisWillStop);
+        }
+    }, 1000)
+}
+
+setTimeout(wakeUpUser, 5000)
+
+function wakeUpUser() {
+    alert("WAKE UUUUUPPP!!!");
+}
+
+// function neverShows() {
+//     let count = 0;
+//     let dontPopUp = setTimeout(function(){
+//         alert("If you see this, you got it wrong :'(");
+//     }, 5000)
+// }
